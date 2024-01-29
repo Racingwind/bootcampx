c50847b797c8df2a7aacdb46b0d5b65285284eab
Racingwind <louis.k.hu@gmail.com>
2024-01-28 17:30:09 -0800

    followed through the tutorial

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+  SELECT DISTINCT(teachers.name) AS name, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = '${process.argv[2]}'
+  ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.name}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
