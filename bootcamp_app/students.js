f2b9b608e0f5640f3719026025a3686d545f01d8
Racingwind <louis.k.hu@gmail.com>
2024-01-28 18:33:14 -0800

    pass sql input from users as sanitized input

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const inputs = [`%${process.argv[2]}%`, process.argv[3]];
+
 pool.query(`
   SELECT students.id AS id, students.name AS name, cohorts.name AS cohort_id
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${process.argv[2]}%'
-  LIMIT ${process.argv[3]};
-`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, inputs)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const inputs = [`%${process.argv[2]}%`];
+
 pool.query(`
   SELECT DISTINCT(teachers.name) AS name, cohorts.name AS cohort
   FROM assistance_requests
   JOIN teachers ON teachers.id = assistance_requests.teacher_id
   JOIN students ON students.id = assistance_requests.student_id
   JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
+  WHERE cohorts.name LIKE $1
   ORDER BY teachers.name;
-`)
+`, inputs)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.name}`);
